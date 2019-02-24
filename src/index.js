ab59f9517ef3273d375fdb8d506c4cc746bc98fa
Breno Lopes <breno_lopes_10@yahoo.com.br>
2019-02-24 19:55:09 -0300

    upload da imagem e armazenamento dos dados no banco de dados mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
+/*
+database setup
+ */
+
+let conexaoMongo = mongoose.connect('mongodb://localhost:27017/upload',{useNewUrlParser:true});
+if(conexaoMongo){
+    console.log("Conexão com banco de dados estabelecida com sucesso!");
+}
+
 app.use(require('./routes'));
 app.use(express.json());
 app.use(express.urlencoded({extended :true}));
